f49e0b37d74b24ef00be45cc7f63dd71198b382a
Fabiano Augusto de Mello <faugmello@proton.me>
2026-04-23 09:25:10 -0300

    exercise 2.9 answer

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,57 +2,77 @@ import {useState} from 'react'
 import {clear} from "@testing-library/user-event/dist/clear";
 
 const App = () => {
+    const [filter, setFilter] = useState('')
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', phone: '12345678'}
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
     ])
+    const [filteredPersons, setFilteredPersons] = useState(persons)
     const [newName, setNewName] = useState('')
-    const [newPhone, setNewPhone] = useState('')
+    const [newNumber, setnewNumber] = useState('')
+
+    const handleFilterChange = (event) => {
+        const newFilter = event.target.value
+        setFilter(newFilter)
+        const filtered = persons.filter(person => person.name.toLowerCase().includes(newFilter))
+        setFilteredPersons(filtered)
+    }
 
     const handleNewNameChange = (event) => {
         setNewName(event.target.value)
     }
 
-    const handleNewPhoneChange = (event) => {
-        setNewPhone(event.target.value)
+    const handleNewNumberChange = (event) => {
+        setnewNumber(event.target.value)
     }
 
     const submitNewPerson = (event) => {
         event.preventDefault()
         const newNameIsAlreadyOnThePhonebook = persons.some(person => person.name === newName)
         const isNameFilled = typeof newName === 'string' && newName.trim().length > 0
-        const isPhoneFilled = typeof newPhone === 'string' && newPhone.trim().length > 0
-        if (!isNameFilled || !isPhoneFilled) {
-            alert('Fill in both the name and the phone!')
+        const isNumberFilled = typeof newNumber === 'string' && newNumber.trim().length > 0
+        if (!isNameFilled || !isNumberFilled) {
+            alert('Fill in both the name and the pwnumberhone!')
         } else if (newNameIsAlreadyOnThePhonebook) {
             alert(`${newName} is already added to phonebook`)
             clearForm()
         } else {
-            setPersons([...persons, { name: newName, phone: newPhone }])
+            const updatedPersons = [...persons, {name: newName, number: newNumber}]
+            setPersons(updatedPersons)
+            setFilteredPersons(updatedPersons)
             clearForm()
         }
     }
 
     const clearForm = () => {
         setNewName('')
-        setNewPhone('')
+        setnewNumber('')
     }
 
     return (
         <div>
             <h2>Phonebook</h2>
+            <form>
+                <div>
+                    filter shown with <input value={filter} onChange={handleFilterChange}/>
+                </div>
+            </form>
+            <h2>add a new</h2>
             <form onSubmit={submitNewPerson}>
                 <div>
                     name: <input value={newName} onChange={handleNewNameChange}/>
                 </div>
                 <div>
-                    number: <input value={newPhone} onChange={handleNewPhoneChange}/>
+                    number: <input value={newNumber} onChange={handleNewNumberChange}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            { persons.map(person => <p key={person.name}>{person.name} {person.phone}</p>) }
+            {filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
         </div>
     )
 }
