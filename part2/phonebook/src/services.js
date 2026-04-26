17fef025e1a4e3bb566524281a2ed34aaf319ccd
Fabiano Augusto de Mello <faugmello@proton.me>
2026-04-26 15:15:10 -0300

    exercise 3.9 answer

--- part2/phonebook/src/services.js
+++ part2/phonebook/src/services.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001'
 
-const getAll = () => axios.get(baseUrl).then(response => response.data)
+const getAll = () => axios.get(`${baseUrl}/api/persons`).then(response => response.data)
 
-const create = newPerson => axios.post(baseUrl, newPerson).then(response => response.data)
+const create = newPerson => axios.post(`${baseUrl}/api/persons`, newPerson).then(response => response.data)
 
-const exclude = id => axios.delete(`${baseUrl}/${id}`).then(response => response.data.id)
+const exclude = id => axios.delete(`${baseUrl}/api/persons/${id}`).then(response => response.data.id)
 
 const update = (updatedPerson) => axios.put(`${baseUrl}/${updatedPerson.id}`, updatedPerson)
     .then(response => response.data)
